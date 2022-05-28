a02b6a5f36a5e24211ca9e4b6e3d25178413220b
HQK <104838129+HQK-11@users.noreply.github.com>
2022-05-28 22:37:22 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+BOT_TOKEN = '5161462975:AAGvGGO37mWCz_Cy1RG-YCf3d2FfxVfmIOU'
+PAYMENTS_TOKEN = '401643678:TEST:d0f28f96-e584-43d9-a615-a951c2f8de27'
+
+item_url = 'https://belms.ru/imagesaver/product/big/belmsru_g1poif9ll45c9seviaep_1621843182.jpg'
